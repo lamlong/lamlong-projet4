10d787707a65985ddc8292294c68011e4a7f9620
ngoclong LAM <31016438+lamlong@users.noreply.github.com>
2024-08-29 11:16:48 +0200

    Add files via upload

---
+++ parkingsystem/test/java/com/parkit/parkingsystem/ParkingServiceTestTest.java
@@ -0,0 +1,14 @@
+package com.parkit.parkingsystem;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ParkingServiceTestTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
